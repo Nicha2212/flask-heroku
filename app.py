e536f281ceb8c8b56af40b8af320c9fee55b20f7
Nicha2212 <89177932+Nicha2212@users.noreply.github.com>
2021-09-02 11:05:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,11 @@ def hello():
 @app.route('/api', methods=['GET'])
 def get_api():
     return jsonify(data)
-
+ 
+@app.route('/hi')
+@app.route('/hello')
+def hi():
+    return "สวัสดี"
+        
 if __name__ == "__main__":
     app.run(debug=False)
