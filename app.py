b847ad9ef295a5d7e36448470995ecce0968ca30
Nicha2212 <89177932+Nicha2212@users.noreply.github.com>
2021-09-02 10:52:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ data = [
 
 @app.route('/')
 def hello():
-    return "Hello Flask-Herok"
+    return "Hello ณิชา อินทรประเสริฐ เลขที่9 ม.4/6"
 
 @app.route('/api', methods=['GET'])
 def get_api():
